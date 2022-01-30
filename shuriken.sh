f8bc58a4c0b470c22e96b089d21d80b1ab0b7ea4
Izul Wahidin <izul@wahid.in>
2022-01-30 23:05:42 +0700

    Update shuriken.sh

--- shuriken.sh
+++ shuriken.sh
@@ -2,7 +2,8 @@
 apt install software-properties-common -y
 add-apt-repository ppa:ondrej/php -y
 apt-get update -y
-apt install -y php7.4 php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath php7.4-sqlite wget curl
+apt install -y php7.4 
+apt install -y php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath php7.4-sqlite wget curl
 
 #printf "\033c"
 echo "======================="
